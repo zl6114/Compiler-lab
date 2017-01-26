4ba039093a70fb34193b0add6d7004cff7034ae8
zl6114 <frankzihanliu@hotmail.com>
2017-01-26 13:47:58 +0000

    better version of b works

--- 1-regexes/b/regex_substitute.cpp
+++ 1-regexes/b/regex_substitute.cpp
@@ -5,15 +5,13 @@
 
 int main(int argc, char *argv[])
 {
-	std::cout << argv[1] << std::endl;
+	//std::cout << argv[1] << std::endl; //testing first input
 	std::basic_regex<char> r(argv[1]);
-	std::string regex_for_slash = "[//]"; //"\\/[[:digit:]]"
-	std::basic_regex<char> replacement(regex_for_slash);
-	std::cout << '\n' << std::regex_replace(argv[2],replacement,"$") << '\n';
-	std::string a = std::regex_replace(argv[2],replacement,"$");
+	std::string a = argv[2];
+	//std::cout << a << std::endl; //testing a
 	std::string tmp;
 	while( std::getline(std::cin, tmp) ){
-		std::cout  << std::regex_replace(tmp, r, a) << '\n';
+		std::cout  << std::regex_replace(tmp, r, a,std::regex_constants::format_sed) << '\n';
    	// do something with line tmp
 	}
     return 0;
