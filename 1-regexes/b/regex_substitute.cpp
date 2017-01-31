3d1c5f08eb5d4fdf0017fa058df971675059c6a6
zl6114 <frankzihanliu@hotmail.com>
2017-01-31 13:07:36 +0000

    better version of b

--- 1-regexes/b/regex_substitute.cpp
+++ 1-regexes/b/regex_substitute.cpp
@@ -6,12 +6,12 @@
 int main(int argc, char *argv[])
 {
 	//std::cout << argv[1] << std::endl; //testing first input
-	std::basic_regex<char> r(argv[1]);
-	std::string a = argv[2];
+	std::basic_regex<char> r(argv[1]); //getting the regex from the input
+	std::string a = argv[2]; 
 	//std::cout << a << std::endl; //testing a
 	std::string tmp;
 	while( std::getline(std::cin, tmp) ){
-		std::cout  << std::regex_replace(tmp, r, a,std::regex_constants::format_sed) << '\n';
+		std::cout  << std::regex_replace(tmp, r, a,std::regex_constants::format_sed) << '\n'; //substitue the string with format sed 
    	// do something with line tmp
 	}
     return 0;
