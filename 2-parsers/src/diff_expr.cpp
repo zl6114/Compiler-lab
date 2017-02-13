a53dd276df7598658d24c314ebf8e325a9ddaff8
zl6114 <frankzihanliu@hotmail.com>
2017-02-13 16:58:27 +0000

    c part works no simplification

---
+++ 2-parsers/src/diff_expr.cpp
@@ -0,0 +1,30 @@
+#include "ast.hpp"
+#include <iomanip>
+
+int main(int argc, char *argv[])
+{
+    //std::string s;
+    //int i = 0;
+    //bool num = true;
+    std::cout << argv[1] << argv[2] << std::endl;
+    /*for(int i = 1; i < argc-1; i++){
+        std::string s(argv[i]);
+        const Expression *ast=parseAST();
+        ast->differentiate(argv[i]);
+        std::cout << s << '\n';
+        std::cout << "+";
+    }*/
+    const Expression *ast=parseAST();
+    for(int i = 1; i < argc; i++){
+        ast->differentiate(argv[i]);
+        //std::cout << i << '\n';
+        if(i != argc-1){
+            std::cout << " + ";
+        }
+    }
+
+
+    std::cout<<std::endl;
+
+    return 0;
+}
